1aa8d4afbfd2fded9712e78fc71ad9fbd9437815
Steve Shaddick <steve@steveshaddick.com>
2013-11-09 22:32:59 -0500

    initial local env

---
+++ melissa_steve/melissa_steve/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render, redirect
+from django.template import Context, loader
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
+from django.conf import settings
+
+
+import os, json
+
+def json_response(success, response={}):
+    
+    if (success is False):
+        if ('error' not in response):
+            response['error'] = 'General error.'
+        response['success'] = False
+    else:
+        response['success'] = True
+
+    return HttpResponse(json.dumps(response), mimetype='application/json')
+
+
+def home(request):
+	response = render(
+        request,
+        'melissa_steve/index.html',
+        {
+            
+        }
+    )
